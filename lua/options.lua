6a007e8e6575795c048d992e604eaf1dcd8ba12b
Bryce Johnson <bryce@Bryces-MacBook-Pro.local>
2025-11-02 23:39:26 -0800

    Set tab size to 2 and enabled tab lines

--- lua/options.lua
+++ lua/options.lua
@@ -50,3 +50,11 @@ vim.o.scrolloff = 10
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+
+-- Set tab size to 2 and use spaces
+vim.o.shiftwidth = 2
+vim.o.smarttab = true
+vim.o.expandtab = true
+
+-- Show tab line
+vim.o.showtabline = 1
