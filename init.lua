be001fbb645470fc896ac18e0ce87a6682f2e095
Bryce Johnson <bryce@Bryces-MacBook-Pro.local>
2025-11-02 17:56:16 -0800

    Started on vim config

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- Set leader to <space>
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Probably don't need a nerd font
+vim.g.have_nerd_font = false
+
+require("config.lazy")
+require("lazy").setup("plugins")
+
+require("options")
