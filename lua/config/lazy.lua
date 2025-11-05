1025306017b3ace776b641d6c46951683e779032
Bryce Johnson <bryce@Bryces-MacBook-Pro.local>
2025-11-04 22:19:21 -0800

    Hid plugin update message

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,9 +23,9 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "tokyonight" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = true, notify = false },
 })
 
 --vim.api.nvim_create_autocmd("VeryLazy", {
